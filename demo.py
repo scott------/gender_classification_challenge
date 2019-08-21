72b7db0a6ad4730a0984d8329594ee65c9c6546b
scott------ <upstatesolutions@gmail.com>
2019-08-21 10:15:35 -0400

    Update demo.py
    added multiple classifiers

--- demo.py
+++ demo.py
@@ -1,26 +1,45 @@
 from sklearn import tree
-
-clf = tree.DecisionTreeClassifier()
+from sklearn.metrics import accuracy_score, log_loss
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC, LinearSVC, NuSVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 
 # CHALLENGE - create 3 more classifiers...
-# 1
-# 2
-# 3
+classifiers = [
+    KNeighborsClassifier(3),
+    SVC(kernel="rbf", C=0.025, probability=True),
+    NuSVC(probability=True),
+    DecisionTreeClassifier(),
+    RandomForestClassifier(),
+    AdaBoostClassifier(),
+    GradientBoostingClassifier(),
+    GaussianNB(),
+    LinearDiscriminantAnalysis(),
+    QuadraticDiscriminantAnalysis()]
 
-# [height, weight, shoe_size]
-X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40],
-     [190, 90, 47], [175, 64, 39],
-     [177, 70, 40], [159, 55, 37], [171, 75, 42], [181, 85, 43]]
 
-Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female',
-     'female', 'male', 'male']
+for clf in classifiers:
+    clf = tree.DecisionTreeClassifier()
+
+    # [height, weight, shoe_size]
+    X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40],
+        [190, 90, 47], [175, 64, 39],
+        [177, 70, 40], [159, 55, 37], [171, 75, 42], [181, 85, 43]]
+
+    Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female',
+        'female', 'male', 'male']
 
 
-# CHALLENGE - ...and train them on our data
-clf = clf.fit(X, Y)
+    # CHALLENGE - ...and train them on our data
+    clf = clf.fit(X, Y)
 
-prediction = clf.predict([[190, 70, 43]])
+    prediction = clf.predict([[190, 70, 43]])
 
-# CHALLENGE compare their reusults and print the best one!
+    # CHALLENGE compare their reusults and print the best one!
 
-print(prediction)
+    print(str(clf) + " Prediction: " + str(prediction))
+    
